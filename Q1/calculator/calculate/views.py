1aba971ca0487493186776b7c9161be740332ab3
ShivaKrishnaReddy Gottimukkula <87538733+G-ShivaKrishna@users.noreply.github.com>
2025-06-12 12:07:48 +0530

    Update views.py

--- Q1/calculator/calculate/views.py
+++ Q1/calculator/calculate/views.py
@@ -17,43 +17,43 @@ def get_token():
     return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzQ5NzA5NTc2LCJpYXQiOjE3NDk3MDkyNzYsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6IjNjZTQ3YjRhLWM0ZGMtNGIxNS1iMzI5LTIxMmQ5NzBmNTIyZiIsInN1YiI6IjIyMDNhNTEyOTFAc3J1LmVkdS5pbiJ9LCJlbWFpbCI6IjIyMDNhNTEyOTFAc3J1LmVkdS5pbiIsIm5hbWUiOiJnb3R0aW11a2t1bGEgc2hpdmEga3Jpc2huYSByZWRkeSIsInJvbGxObyI6IjIyMDNhNTEyOTEiLCJhY2Nlc3NDb2RlIjoiTVZHd0VGIiwiY2xpZW50SUQiOiIzY2U0N2I0YS1jNGRjLTRiMTUtYjMyOS0yMTJkOTcwZjUyMmYiLCJjbGllbnRTZWNyZXQiOiJFVVRVZ1F0eGRTbXdraGhjIn0.hklGo4XQ1PlXmjdhf3ygaD9ntFPtVii1jbrLFmM0UX0"
 
 def fetch_numbers_from_api(identifier):
-    token = get_token()
+    token=get_token()
     if not token:
         return []
     try:
-        url = VALID_IDS[identifier]
-        headers = {
+        url=VALID_IDS[identifier]
+        headers={
             'Authorization':f'Bearer {token}'
         }
-        res = requests.get(url, headers=headers, timeout=2)
+        res=requests.get(url, headers=headers, timeout=2)
         if res.status_code==200:
-            return res.json().get("numbers", [])
+            return res.json().get("numbers",[])
     except:
         pass
     return []
 
 def calculate_average(numbers):
-    return round(sum(numbers)/len(numbers), 2) if numbers else 0.0
+    return round(sum(numbers)/len(numbers),2) if numbers else 0.0
 
 @api_view(['GET'])
-def number_handler_view(request, identifier):
+def number_handler_view(request,identifier):
     if identifier not in VALID_IDS:
         return Response({"error": "Invalid ID"}, status=status.HTTP_400_BAD_REQUEST)
 
-    previous_state = list(number_windows[identifier])
-    new_numbers = fetch_numbers_from_api(identifier)
+    previous_state=list(number_windows[identifier])
+    new_numbers=fetch_numbers_from_api(identifier)
 
     for num in new_numbers:
         if num not in number_windows[identifier]:
             number_windows[identifier].append(num)
-        if len(number_windows[identifier]) == WINDOW_SIZE:
+        if len(number_windows[identifier])==WINDOW_SIZE:
             break
 
-    current_state = list(number_windows[identifier])
-    average_value = calculate_average(current_state)
+    current_state=list(number_windows[identifier])
+    average_value=calculate_average(current_state)
 
     return Response({
-        "windowPrevState": previous_state,
-        "windowCurrState": current_state,
-        "avg": average_value
+        "windowPrevState":previous_state,
+        "windowCurrState":current_state,
+        "avg":average_value
     }, status=status.HTTP_200_OK)
